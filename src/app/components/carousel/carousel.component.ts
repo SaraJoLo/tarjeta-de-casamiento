a2a42756de0362716f2a432d7c90b3276fdeb2a6
SaraJoLo <162146662+SaraJoLo@users.noreply.github.com>
2025-05-05 20:30:54 -0300

    carousel component complete

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
@@ -7,6 +7,20 @@ import { Component } from '@angular/core';
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.scss'
 })
-export class CarouselComponent {
+export class CarouselComponent implements OnInit, OnDestroy {
+  images: string[] = [];
+  currentIndex: number = 0;
+  intervalId: any;
 
+  ngOnInit(): void {
+    this.images = Array.from({ length: 10 }, (_, i) => `assets/img/img${i + 1}.jpg`);
+    
+    this.intervalId = setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    }, 3000); 
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
+  }
 }
